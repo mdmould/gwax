b6cd39532f31a61ec28e52d0211712b4aa5af940
MDM988 <mattdmould@gmail.com>
2024-07-11 19:21:00 -0400

    new more general training loop and update function

--- gwax/neural/train.py
+++ gwax/neural/train.py
@@ -4,35 +4,43 @@ import jax_tqdm
 import equinox
 
 
+@equinox.filter_jit
+def update(params, loss_fn, optimizer, state, *args):
+    loss_and_grad = equinox.filter_value_and_grad(loss_fn, has_aux = True)
+    (loss, *args), grad = loss_and_grad(params, *args)
+    updates, state = optimizer.update(grad, state, params)
+    params = equinox.apply_updates(params, updates)
+    return params, state, loss, *args
+
+
 def trainer(
-    key,
     params,
     loss_fn,
     optimizer,
     steps,
+    *args,
     print_rate = 1,
 ):
+    def _update(params, state, *args):
+        return update(params, loss_fn, optimizer, state, *args)
+
     @jax_tqdm.scan_tqdm(steps, print_rate = print_rate)
     @equinox.filter_jit
-    def update(carry, step):
-        key, params, state, best_loss, best_params = carry
-        new_key, key = jax.random.split(key)
-        loss, grad = equinox.filter_value_and_grad(loss_fn)(params, key, step)
-        updates, state = optimizer.update(grad, state, params)
-        new_params = equinox.apply_updates(params, updates)
+    def loop(carry, step):
+        params, state, best_loss, best_params, *args = carry
+        new_params, state, loss, *args = _update(params, state, *args)
         best_loss, best_params = jax.lax.cond(
             loss < best_loss,
             lambda: (loss, params),
             lambda: (best_loss, best_params),
         )
-        carry = new_key, new_params, state, best_loss, best_params
-        return carry, loss
+        return (new_params, state, best_loss, best_params, *args), loss
 
-    (key, last_params, state, best_loss, best_params), losses = jax.lax.scan(
-        update,
-        (key, params, optimizer.init(params), jnp.inf, params),
+    (params, state, best_loss, best_params, *args), losses = jax.lax.scan(
+        loop,
+        (params, optimizer.init(params), jnp.inf, params, *args),
         jnp.arange(steps),
     )
     
-    return losses, last_params, best_params
+    return losses, params, best_params
 
