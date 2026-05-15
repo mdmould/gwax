582a379dd754f1c021f3ea85c2b81239423c910c
Matthew Mould <mattdmould@gmail.com>
2026-05-15 19:19:52 +0000

    renaming in nonparametric

--- gwax/nonparametric.py
+++ gwax/nonparametric.py
@@ -16,16 +16,16 @@ def get_adjacent(*shape):
         pairs_list.append(np.stack([a, b], axis=1))
     return jnp.vstack(pairs_list)
 
-def get_bins_1d(samples, edges):
-    return jnp.clip(jnp.digitize(samples, edges) - 1, 0, len(edges) - 2)
-
-def get_bins_nd(samples, edges):
+def get_bins(samples, edges):
     multi_index = [
         jnp.clip(jnp.digitize(x, e) - 1, 0, len(e) - 2)
         for x, e in zip(samples, edges)
     ]
     dims = [len(e) - 1 for e in edges]
     return jnp.ravel_multi_index(multi_index, dims)
+
+def get_bins_1d(samples, edges):
+    return jnp.clip(jnp.digitize(samples, edges) - 1, 0, len(edges) - 2)
 
 def icar_penalty(adj, y):
     return jnp.sum(jnp.diff(y[adj], axis = 1) ** 2) / 2
@@ -35,7 +35,7 @@ def ln_prior_icar(adj, y, tau):
     ln_prior = jnp.log(tau) * (y.size - 1) / 2 - penalty * tau
     return ln_prior
 
-def ln_prior_icar_gamma_marginalized_tau(adj, y, a, b):
+def ln_prior_icar_gamma(adj, y, a, b):
     penalty = icar_penalty(adj, y)
     ln_prior = -(a + (y.size - 1) / 2) * jnp.log(b + penalty)
     return ln_prior
